f820ec6544704ec5c33a925efd87ac287695a18f
Raphael Fleury <raphael.fleury9@gmail.com>
2024-02-08 21:40:32 -0300

    Express installed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world')
+import express from 'express'
+
+const port = process.env.PORT || 4000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send({ message: "Hello World" })
+})
+
+app.use((req, res) => {
+    res.status(404)
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando com sucesso na porta ${port}`)
+})
