9ba220222abafc7e0f0a28e591ceb8a2100f1ec6
Raphael Fleury <raphael.fleury9@gmail.com>
2024-02-11 18:53:23 -0300

    Using CORS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import clientesRouter from './routers/clientesRouter'
 import vendasRouter from './routers/vendasRouter'
 
 const port = process.env.PORT || 4000
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
